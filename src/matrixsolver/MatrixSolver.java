4c2284846e7aa3872e4c7808239454b204a032b4
xum3131 <xum3131@JAM-IC0037499.wrdsb.ca>
2016-12-22 10:43:30 -0500

    initialized

---
+++ src/matrixsolver/MatrixSolver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrixsolver;
+
+/**
+ *
+ * @author xum3131
+ */
+public class MatrixSolver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
