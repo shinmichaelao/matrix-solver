e214f372c16745c39be2d8ee617888bbb5cb9401
xum3131 <xum3131@JAM-IC0037499.wrdsb.ca>
2016-12-22 11:01:39 -0500

    initialized

---
+++ src/matrixsolver/Matrix.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrixsolver;
+
+/**
+ *
+ * @author xum3131
+ */
+public class Matrix {
+    
+}
