0ce991fefb9d43d2f5612d04aa64183984dbeccf
xum3131 <xum3131@JAM-IC0037499.wrdsb.ca>
2017-01-17 11:02:27 -0500

    Row-Echelon Form finallllllllllly

--- src/matrixsolver/MatrixSolver.java
+++ src/matrixsolver/MatrixSolver.java
@@ -15,14 +15,17 @@ public class MatrixSolver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Row kappa = new Row("6x - 3y + z = x");
-        Row kappa2 = new Row("x + y - z + a= 0");
+        Row kappa = new Row("2x + y - z = 8");
+        Row kappa2 = new Row("-3x -y +2z = -11");
+        Row kappa3 = new Row("-2x +y +2z = -3");
         List<Row> keepo = new ArrayList<>();
         keepo.add(kappa);
         keepo.add(kappa2);
+        keepo.add(kappa3);
         Matrix m = new Matrix(keepo);
         System.out.println(m);
-        m.add(0, 1);
+        m.solve();
         System.out.println(m);
+        
     }
 }
